cdad94571c4841bec19d82e491dc572fc3345032
Taylor <Taylor@VM-LAB-DEV>
2024-11-11 10:39:24 +0000

    Add project files.

---
+++ WebRole1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRole1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Página de Inicio";
+            ViewBag.ApplicationName = "Created by Taylor Balmaceda";
+            return View();  // Renderiza la vista Index.cshtml
+        }
+
+        public ActionResult Info()
+        {
+            ViewBag.Title = "Información";
+            ViewBag.ApplicationName = "Created by Taylor Balmaceda";
+            return View();
+        }
+    }
+}
+
+
---
+++ WebRole1/Scripts/navbar.js
@@ -0,0 +1,14 @@
+﻿// Función para hacer que la barra de navegación sea sticky
+window.onscroll = function () { stickyNavbar() };
+
+var navbar = document.querySelector('.navbar'); // Seleccionamos la barra de navegación
+var sticky = navbar.offsetTop; // Obtenemos la posición superior de la barra
+
+// Función que se activa cuando se hace scroll
+function stickyNavbar() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky"); // Añadimos la clase sticky cuando se pasa de la posición inicial
+    } else {
+        navbar.classList.remove("sticky"); // Elimina la clase sticky cuando se vuelve al principio
+    }
+}
